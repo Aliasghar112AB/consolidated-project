c63f49640b511c5cd29f27e81368aa75fe25bb81
Aliasghar112AB <abbasi.aliasghar1386@gmail.com>
2024-07-16 22:46:58 +0330

    Main file you can access all the projects from here

---
+++ Main.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "Calculator/project/Calculator.h"
+#include "number-guessing-game/project/NumberGuessingGame.h"
+#include "output-input/project/outputInput.h"
+#include "Functions/project/Functions.h"
+#include "String/project/String_.h"
+#include "Arrays/project/Arrays.h"
+#include "Struct-union/project/Struct-Union.h"
+#include "StudentInfo/project/StudentInfo.h"
+#include "ReadFile/project/ReadFile.h"
+
+
+int main(void) {
+    printf("Hello World!\n");
+
+    return 0;
+}

a379b93d9049411aac32c7304a030c254f4bca5e
Aliasghar112AB <abbasi.aliasghar1386@gmail.com>
2024-07-16 22:54:00 +0330

    Header File

---
+++ Functions/project/Functions.h
@@ -0,0 +1,8 @@
+//
+// Created by abbas on 7/16/2024.
+//
+
+#ifndef CONSOLIDATED_PROJECT_FUNCTIONS_H
+#define CONSOLIDATED_PROJECT_FUNCTIONS_H
+void Functions();
+#endif //CONSOLIDATED_PROJECT_FUNCTIONS_H
